1f8dc0284e627e16bc1afc20c3d012e398208b3e
Dominik <156206194+Domjel12@users.noreply.github.com>
2025-01-14 15:18:41 +0100

    Update Ü7.sql

--- Ü7/Ü7.sql
+++ Ü7/Ü7.sql
@@ -1,52 +1,102 @@
---Exercise 7
+-- Exercise 7
 --------------------------------------------------------------------------------------------
-# 1. Try to DELETE a dataset of your zip table on ...
+1.	Try to DELETE a dataset of your zip table on …
 --------------------------------------------------------------------------------------------
-	-- a. ... which you have a reference out of your table person.
-    
-DELETE FROM zip
-WHERE ID = 2036;
+-- a. … which you have a reference out of your table person.
+	
+SELECT DISTINCT zip_id
+FROM person
+;
 
-	-- b. ... which you haven’t got a reference out of your table person.
+-- looking which zip_ids are used
+-- RESULT:	10034 & 10035 & 10036 & 10038 & 10040 & 10041 & 10042 & 10044 & 10082
 
-DELETE FROM zip
-WHERE ID = 97;
+DELETE 
+FROM zip
+WHERE id = 10082
+;
+-- LÖSCHUNG wird nicht durchgeführt!
 
-	-- What is happening and why?
-		-- The first one is giving an error because we set on delete/update restrict, and since we are trying to delete
-			-- a reference to the table person
-		-- The second one is not giving an error since there is no reference to the person table
+-- b. … which you haven’t got a reference out of your table person.
+
+SELECT *
+FROM zip
+WHERE id = 14957
+;
+
+DELETE 
+FROM zip
+WHERE id = 14957
+;
+-- LÖSCHUNG wird durchgeführt!
+
+-- ROLLBACK 
+INSERT INTO zip
+VALUES (14957, 1, 'Weinbergen', '99998')
+;
+
 --------------------------------------------------------------------------------------------
-# 2. Table gender
+2. Table gender
 --------------------------------------------------------------------------------------------
-	-- a. CREATE a new table with the following setting:
-    
-CREATE TABLE gender (
-	id INT NOT NULL AUTO_INCREMENT,
-    gender VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-	-- b. Add in your table the following two attrbutes
-
+-- a. CREATE a new Table gender with the following settings:
+CREATE TABLE gender
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    gender VARCHAR(20) NOT NULL
+)
+;
+-- b. Add in your table gender the following two attributes:
 INSERT INTO gender (gender)
-VALUES ('Mr.'), ('Mrs.');
+VALUES ('Mrs.'), ('Mr.')
+;
+-- works fine, but if we look at the ids ...
+
+SELECT *
+FROM gender
+;
+
+TRUNCATE TABLE gender;
+
+-- .. so we have to change the ids :)
 
 UPDATE gender
 SET id = 0
-WHERE gender = 'Mr.';
+WHERE gender = 'Mrs.'
+;
 
 UPDATE gender
 SET id = 1
-WHERE gender = 'Mrs.';
+WHERE gender = 'Mr.'
+;
 
-	-- c. Add now a constraint with the name fk_person2gender and
-	-- set the gender as a FOREIGN KEY from the table person which REFERENCES to the id from gender.
-    -- Please use RESTRICT for UPDATE & DELETE!
-
+-- c. Add now a constraint with the name fk_person2gender and set the gender as a FOREIGN KEY from the table person which REFERENCES to the id from gender. Please use RESTRICT for UPDATE & DELETE!
 ALTER TABLE person
 ADD CONSTRAINT fk_person2gender
 FOREIGN KEY (gender)
-REFERENCES gender(id)
-	ON DELETE RESTRICT
-    ON UPDATE CASCADE;
+REFERENCES gender (id)
+ON DELETE RESTRICT
+ON UPDATE CASCADE
+;
+
+SELECT * FROM person;
+-- 9 Attribute
+-- 11 Datensätze
+
+SELECT * FROM gender;
+-- 2 Attribute
+-- 2 Datensätze
+
+-- Cardinality (datasets): 	2 * 11 = 22
+-- Grade (attributes): 		2 + 9 = 11
+
+SELECT *
+FROM gender, person
+;
+-- Exkurs: JOIN EXAMPLE
+-- How can we now combine our three tables (gender, person, zip)
+
+SELECT g.gender, p.nnam, p.fnam, p.str, z.zip_code, z.city
+FROM person p
+INNER JOIN gender g ON g.id = p.gender
+INNER JOIN zip z ON z.id = p.zip_id
+;
